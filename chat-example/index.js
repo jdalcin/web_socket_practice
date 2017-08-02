459fd4e7fc382e9fadb699472fbd7bcc47fa2a02
Ubuntu <ubuntu@ip-172-31-30-144.us-east-2.compute.internal>
2017-08-02 03:15:01 +0000

    Socket.io that places circles in random spots when screen is clicked

---
+++ chat-example/index.js
@@ -0,0 +1,50 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var createCanvas = require('canvas');
+
+var canvas = new createCanvas(50, 50);
+var context = canvas.getContext('2d');
+var number = 0;
+
+// draw circle in canvas
+function drawCircle() {
+	var centerX = canvas.width / 2;
+	var centerY = canvas.height / 2;
+	var radius = canvas.width / 3;
+
+
+	context.beginPath();
+	context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+	context.fillStyle = 'blue';
+	context.fill();
+	console.log("Canvas Updated.");
+}
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  // socket.on('chat message', function(msg){
+  //   console.log('message: ' + msg);
+  //   io.emit('chat message', msg);
+  // });
+  socket.on('clientClick', function(data) {
+  	console.log("Click reached server.")
+  	drawCircle();
+  	number++;
+  	io.emit('clientClick', {URL: canvas.toDataURL(), numShapes: number});
+  	console.log("Server sent click data.");
+  	console.log("Number of images is: " + number);
+  })
+});
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
