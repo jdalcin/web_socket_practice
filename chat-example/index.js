46126c63e70b6e298eb53f1dde7460ba02148b71
Ubuntu <ubuntu@ip-172-31-30-144.us-east-2.compute.internal>
2017-08-02 18:45:13 +0000

    Allows circles to be enlarged when a user holds a mouse button down

--- chat-example/index.js
+++ chat-example/index.js
@@ -3,47 +3,62 @@ const http = require('http').Server(app);
 var io = require('socket.io')(http);
 var createCanvas = require('canvas');
 
-var canvas = new createCanvas(50, 50);
-var context = canvas.getContext('2d');
-var number = 0;
+// server global variables
+  var numShapes = 0;
 
 // draw circle in canvas
-function drawCircle() {
-	var centerX = canvas.width / 2;
-	var centerY = canvas.height / 2;
-	var radius = canvas.width / 3;
+function drawCircle(radius, canvas) {
+  var centerX = canvas.width / 2;
+  var centerY = canvas.height / 2;
+  var context = canvas.getContext('2d');
 
-
-	context.beginPath();
-	context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-	context.fillStyle = 'blue';
-	context.fill();
-	console.log("Canvas Updated.");
+  context.beginPath();
+  context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+  context.fillStyle = 'blue';
+  context.fill();
+  console.log("Canvas Updated.");
 }
 
+// sets up html and confirms web socket connections
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('mouseDown', function(data) {
+    console.log("Mouse held down has reached server.");
+    var canvas = null;
+    var x = 0;
+    var y = 0;
+    if (data.clientShapeID === 0) {
+      numShapes++;
+      console.log("Number of shapes updated to: " + numShapes);
+      data.clientShapeID = numShapes;
+      canvas = new createCanvas(50, 50);
+      data.clientShapeRadius = canvas.width / 3;
+       x = Math.random() * data.screenWidth;
+       y = Math.random() * data.screenHeight;
+    } else {
+      canvas = new createCanvas(data.clientShapeRadius * 2 + 2, data.clientShapeRadius * 2 + 2);
+      data.clientShapeRadius = data.clientShapeRadius + 1;
+      x = data.clientShapeXPos;
+      y = data.clientShapeYPos;
+    }
+    drawCircle(data.clientShapeRadius, canvas);
+    console.log(data.clientShapeRadius);
+    io.emit('mouseDown', {
+      serverShapeURL: canvas.toDataURL(),
+      serverShapeID: data.clientShapeID,
+      serverShapeRadius: data.clientShapeRadius,
+      serverXPos: x,
+      serverYPos: y
+    });
+    console.log("Server sent data to client with mouse pushed down.");
+  });
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-  // socket.on('chat message', function(msg){
-  //   console.log('message: ' + msg);
-  //   io.emit('chat message', msg);
-  // });
-  socket.on('clientClick', function(data) {
-  	console.log("Click reached server.")
-  	drawCircle();
-  	number++;
-  	var x = Math.random() * data.screenWidth;
-          	var y = Math.random() * data.screenHeight;
-  	io.emit('clientClick', {URL: canvas.toDataURL(), numShapes: number, x: x, y: y});
-  	console.log("Server sent click data.");
-  	console.log("Number of images is: " + number);
-  })
 });
 
 
